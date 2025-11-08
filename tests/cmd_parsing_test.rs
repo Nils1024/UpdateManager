bfa9f53c9a1785da1f88c6cd344dcd85fd0bcb38
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-11-08 23:04:52 +0100

    Removed bad test and added a new test

--- tests/cmd_parsing_test.rs
+++ tests/cmd_parsing_test.rs
@@ -1,14 +1,7 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn test_add() {
         assert_eq!(1 + 2, 3);
-    }
-
-    #[test]
-    fn test_bad_add() {
-        assert_eq!(1 + 7, 3);
     }
 }
---
+++ tests/config_test.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    use update_manager::util;
+
+    #[test]
+    fn test_string_building() {
+        assert_eq!(util::constants::PROGRAM_NAME.to_owned() + util::constants::CONFIG_FILE_EXTENSION, "upman.cfg");
+    }
+}
