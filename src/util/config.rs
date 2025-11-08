81c1913a0a31a30ae3902a37250630b4afb5b17e
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-11-08 23:07:05 +0100

    Implemented some config code

--- src/util/config.rs
+++ src/util/config.rs
@@ -1,9 +1,19 @@
-use std::path::Path;
+use std::{fs::{File}, path::Path};
+
+use crate::util;
+
+pub fn does_config_exists() -> bool {
+    return Path::new(&get_config_name()).exists();
+}
 
 pub fn read_config(path: &Path) {
 
 }
 
 pub fn write_default_config(path: &Path) {
+    let mut file = File::create(get_config_name());
+}
 
+fn get_config_name() -> String {
+    return util::constants::PROGRAM_NAME.to_owned() + util::constants::CONFIG_FILE_EXTENSION;
 }
