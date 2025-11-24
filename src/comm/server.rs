7d8be481b0288f3ea09e07bf54e1d27db5c0efeb
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-11-24 22:33:37 +0100

    Added that the used port can be selected

--- src/comm/server.rs
+++ src/comm/server.rs
@@ -1,32 +1,19 @@
-use std::io::{Error, ErrorKind};
 use std::net::TcpListener;
 use std::path::Path;
 use crate::comm::conn::Conn;
 use crate::comm::conn_event::{ConnEvent, ConnEventType};
 use crate::util;
+use crate::util::config::get_config;
 
 /// Starts the server and waits for incoming connections.
 pub fn start() -> std::io::Result<()> {
-    let binding = util::config::get_config_name();
-    let path = Path::new(&binding);
-
-    if util::config::does_config_exists() {
-        util::config::read_config(path)
-    } else {
-        match util::config::write_default_config(path) {
-            Ok(_) => util::config::read_config(path),
-            Err(_) => return Err(Error::from(ErrorKind::InvalidData)),
-        }
-    }
-
-    let socket = TcpListener::bind("127.0.0.1:1234")?;
+    let socket = TcpListener::bind(format!("127.0.0.1:{}", get_config().get("port").unwrap()))?;
     let mut connections = Vec::new();
 
     for stream in socket.incoming() {
         let new_conn = Conn::new(stream?);
 
         println!("New client");
-        new_conn.send_msg("Hello\n".to_owned());
 
         if let Ok(mut publisher) = new_conn.events() {
             publisher.subscribe(ConnEventType::MsgReceived, received_message);
@@ -41,6 +28,6 @@ pub fn received_message(event: ConnEvent) {
     println!("Message received: {}", event.payload);
 
     if event.payload == "ClientHello\n" {
-        util::hash::get_dir_hash(Path::new("./"));
+        event.source.send_msg(util::hash::get_dir_hash(Path::new("./")));
     }
 }
--- src/util/config.rs
+++ src/util/config.rs
@@ -1,23 +1,65 @@
 use std::{fs, fs::{File}, io, path::Path};
-use std::io::Write;
+use std::collections::HashMap;
+use std::io::{Write};
+use std::sync::OnceLock;
+use json::{object, JsonValue};
 use crate::util;
 
-pub fn does_config_exists() -> bool {
-    return Path::new(&get_config_name()).exists();
+static CONFIG: OnceLock<HashMap<String, String>> = OnceLock::new();
+
+pub fn get_config() -> &'static HashMap<String, String> {
+    CONFIG.get_or_init(|| {
+        let mut map = HashMap::new();
+
+        init_config(&mut map);
+
+        map
+    })
 }
 
-pub fn read_config(path: &Path) {
+fn init_config(config_map: &mut HashMap<String, String>) {
+    let binding = get_config_name();
+    let path = Path::new(&binding);
+
+    if does_config_exists() {
+        let config = read_config(path);
+
+        for entry in config.entries() {
+            config_map.insert(entry.0.to_string(), entry.1.to_string());
+        }
+    } else {
+        match write_default_config(path) {
+            Ok(_) => init_config(config_map),
+            Err(_) => panic!("Unable to create default config file"),
+        }
+    }
+}
+
+pub fn does_config_exists() -> bool {
+    Path::new(&get_config_name()).exists()
+}
+
+pub fn read_config(path: &Path) -> JsonValue{
     if let Ok(content) = fs::read_to_string(path) {
-        println!("{:?}", json::parse(&content).unwrap());
+        json::parse(&content).unwrap()
     } else {
         eprintln!("Failed to read config file");
+        JsonValue::Null
     }
 }
 
 pub fn write_default_config(path: &Path) -> io::Result<File> {
     let mut file = File::create(path)?;
-    file.write_all(b"{}")?;
+    file.write_all(get_default_config().to_string().as_bytes())?;
     Ok(file)
+}
+
+fn get_default_config() -> JsonValue {
+    let default_data = object! {
+        "port" => 4455,
+    };
+
+    default_data
 }
 
 pub fn get_config_name() -> String {
