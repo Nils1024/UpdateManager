7152caa2cb48786c1efa935725ccce356b4900f4
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-11-24 23:43:25 +0100

    Added that the hash is calculated once

--- src/comm/server.rs
+++ src/comm/server.rs
@@ -1,33 +1,53 @@
-use std::net::TcpListener;
+use std::net::{TcpListener};
 use std::path::Path;
 use crate::comm::conn::Conn;
 use crate::comm::conn_event::{ConnEvent, ConnEventType};
 use crate::util;
-use crate::util::config::get_config;
 
-/// Starts the server and waits for incoming connections.
-pub fn start() -> std::io::Result<()> {
-    let socket = TcpListener::bind(format!("127.0.0.1:{}", get_config().get("port").unwrap()))?;
-    let mut connections = Vec::new();
-
-    for stream in socket.incoming() {
-        let new_conn = Conn::new(stream?);
-
-        println!("New client");
-
-        if let Ok(mut publisher) = new_conn.events() {
-            publisher.subscribe(ConnEventType::MsgReceived, received_message);
-        }
-        connections.push(new_conn);
-    }
-
-    Ok(())
+pub struct Server {
+    dir_hash: String,
+    socket: TcpListener,
 }
 
-pub fn received_message(event: ConnEvent) {
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Server {
+            dir_hash: util::hash::get_dir_hash(Path::new("./")),
+            socket: TcpListener::bind(addr).unwrap(),
+        }
+    }
+
+    /// Starts the server and waits for incoming connections.
+    pub fn start(&self) -> std::io::Result<()> {
+        let mut connections = Vec::new();
+
+        for stream in self.socket.incoming() {
+            let new_conn = Conn::new(stream?);
+
+            println!("New client");
+
+            let hash = self.get_hash();
+
+            if let Ok(mut publisher) = new_conn.events() {
+                publisher.subscribe(ConnEventType::MsgReceived, move |event| {
+                    received_message(event, &hash);
+                });
+            }
+            connections.push(new_conn);
+        }
+
+        Ok(())
+    }
+
+    pub fn get_hash(&self) -> String {
+        self.dir_hash.clone()
+    }
+}
+
+fn received_message(event: ConnEvent, hash: &str) {
     println!("Message received: {}", event.payload);
 
     if event.payload == "ClientHello\n" {
-        event.source.send_msg(util::hash::get_dir_hash(Path::new("./")));
+        event.source.send_msg(hash.to_string());
     }
 }
--- src/server_starter.rs
+++ src/server_starter.rs
@@ -1,5 +1,11 @@
-use update_manager::comm::server::start;
+use update_manager::comm::server::{Server};
+use update_manager::util::config::get_config;
 
 fn main() {
-    start().expect("Server stopped");
+    let server = Server::new(format!("127.0.0.1:{}", get_config().get("port").unwrap()));
+
+    println!("Server started at {}", get_config().get("port").unwrap());
+    println!("Hash: {}", server.get_hash());
+
+    server.start().expect("Server stopped");
 }
