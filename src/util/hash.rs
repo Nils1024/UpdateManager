ae5c9289e6cdbbfeca5d8c0b8c833f6ca753496b
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-12-04 22:14:37 +0100

    Removed config file from hash calculation

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::fs::{self, DirEntry};
 use std::path::Path;
-
+use crate::util;
 use crate::util::files::walk_file_tree;
 
 // Returns a single hash of all elements in the directory
@@ -9,6 +9,10 @@ pub fn get_dir_hash(dir: &Path) -> String {
     let hashes = RefCell::new(Vec::new());
 
     walk_file_tree(dir, &|entry| {
+        if entry.file_name().to_str().unwrap() == util::config::get_config_name() {
+            return;
+        }
+        
         hashes.borrow_mut().push(get_file_hash(entry));
     }).unwrap();
 
