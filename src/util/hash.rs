37deab2756c3723cc4170e78aaf3a7b5d326aa68
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-12-05 20:28:39 +0100

    Removed unnecessary return statements

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use crate::util;
 use crate::util::files::walk_file_tree;
 
-// Returns a single hash of all elements in the directory
+/// Returns a single hash of all elements in the directory
 pub fn get_dir_hash(dir: &Path) -> String {
     let hashes = RefCell::new(Vec::new());
 
@@ -19,10 +19,10 @@ pub fn get_dir_hash(dir: &Path) -> String {
     let mut hashes = hashes.into_inner();
     hashes.sort();
 
-    return sha256::digest(hashes.join(""));
+    sha256::digest(hashes.join(""))
 }
 
 /// Returns the hash of the file by reading its content
 fn get_file_hash(file: &DirEntry) -> String {
-    return sha256::digest(fs::read(file.path()).unwrap());
+    sha256::digest(fs::read(file.path()).unwrap())
 }
