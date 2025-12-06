959361bd0877b00f574a0ebe3fbccbbf7bb2d3df
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-12-06 23:49:46 +0100

    added is_excluded function

--- src/util/files.rs
+++ src/util/files.rs
@@ -1,6 +1,6 @@
-use std::io;
+use std::{env, io};
 use std::fs::{self, DirEntry};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 /// Walks down the entire file tree and visits every file
 pub fn walk_file_tree(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
@@ -20,4 +20,32 @@ pub fn walk_file_tree(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
 
 pub fn print_file_name(file: &DirEntry) {
     println!("Name: {}", file.file_name().to_string_lossy());
+}
+
+pub fn is_excluded(entry: &DirEntry) -> bool {
+    if let Some(name) = entry.path().file_name().and_then(|s| s.to_str()) {
+        if name == "upman.json" || name == ".DS_Store" {
+            return true
+        }
+    }
+
+    let exe_canon: PathBuf  = match env::current_exe().and_then(|p| fs::canonicalize(p)) {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Unable to get current_exe: {}", e);
+            return true
+        }
+    };
+
+    let entry_canon = match fs::canonicalize(entry.path()) {
+        Ok(c) => c,
+        Err(_) => {
+            return true;
+        }
+    };
+    if entry_canon == exe_canon {
+        return true
+    }
+
+    false
 }
