5e8d8b46d14ac86e643ef00452c2f75d205ae43c
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-12-08 20:08:59 +0100

    Added a warning when no config is found

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,29 @@
-use update_manager::comm;
+use std::io::stdin;
+use std::process::exit;
+use update_manager::{comm, util};
+use update_manager::util::config::get_config;
 
 fn main() {
-    comm::client::connect();
+    if !util::config::does_config_exists() {
+        get_config();
+
+        if !util::config::does_config_exists() {
+            eprintln!("Failed to create config file");
+            return;
+        } else {
+            println!("Config file created. Would you like to continue? [y/n]");
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Did not enter a correct string");
+
+            if input.trim().to_lowercase() == "y" {
+                exit(1);
+            }
+        }
+    }
+
+    let connection_result = comm::client::connect();
+
+    if !connection_result {
+        eprintln!("Failed to connect to server. Make sure the server is running or you configured the correct address in the upman.json");
+    }
 }
