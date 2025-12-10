c12aeb562913ecb65a5b40bbdd62f3b29292ca0b
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-12-10 23:24:12 +0100

    Added new file transfer

--- src/comm/client.rs
+++ src/comm/client.rs
@@ -4,13 +4,26 @@ use std::net::TcpStream;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, Ordering};
 use crate::comm::conn::Conn;
 use crate::comm::conn_event::{ConnEventType};
 use crate::comm::conn_state::ConnState;
 use crate::comm::session::Session;
 use crate::util;
 use crate::util::config::get_config;
+
+struct Metadata {
+    size: usize,
+    name: String,
+    permissions: Permissions
+}
+
+struct FileTransfer {
+    metadata: Option<Metadata>,
+    received: usize,
+    file_stream: Option<BufWriter<File>>,
+    buffer: Vec<u8>,
+}
 
 pub fn connect() -> bool {
     let addr = format!(
@@ -37,12 +50,17 @@ pub fn connect() -> bool {
 
     let session_ref = Arc::new(Mutex::new(session));
     let is_meta_data = Arc::new(AtomicBool::new(true));
-    let remaining_bytes = Arc::new(AtomicUsize::new(0));
-    let file_stream = Arc::new(Mutex::new(None::<BufWriter<File>>));
-    let metadata_buffer = Arc::new(Mutex::new(Vec::new()));
+    let file_transfer_data = Arc::new(Mutex::new(None::<FileTransfer>));
+
+    *file_transfer_data.lock().unwrap() = Some(FileTransfer {
+        metadata: None,
+        buffer: Vec::new(),
+        received: 0,
+        file_stream: None,
+    });
 
     if let Ok(mut publisher) = conn.events() {
-        publisher.subscribe(ConnEventType::MsgReceived, move |event| {
+        publisher.subscribe(ConnEventType::MsgReceived, move |mut event| {
             println!("Message received: {:?}", event.payload);
 
             if let Ok(mut current_session) = session_ref.lock() {
@@ -56,101 +74,35 @@ pub fn connect() -> bool {
                         event.source.close();
                     }
                 } else if current_session.state == ConnState::Update {
+                    let mut guard = file_transfer_data.lock().unwrap();
+                    let current_file_transfer = guard.as_mut().unwrap();
+
                     if is_meta_data.load(Ordering::Acquire) {
-                        if let Ok(meta_data) = json::parse(&*String::from_utf8_lossy(&event.payload[0..(event.payload.len() - 1)])) {
-                            if let Some(size_val) = meta_data["size"].as_usize() {
-                                remaining_bytes.store(size_val, Ordering::Release);
-                            }
-                            if let Some(name_val) = meta_data["name"].as_str() {
-                                let file = File::create(name_val).unwrap();
-
-                                if let Some(is_app_val) = meta_data["is_app"].as_bool() {
-                                    if is_app_val {
-                                        file.set_permissions(Permissions::from_mode(0o111)).expect("Failed to set permissions");
-                                    }
-                                }
-
-                                *file_stream.lock().unwrap() = Some(BufWriter::new(file));
-                            }
-
+                        if let Some(meta_data) = get_meta_data(&event.payload) {
+                            current_file_transfer.metadata = Some(meta_data);
                             is_meta_data.store(false, Ordering::Release);
-                        }
-                    } else {
-                        let remaining_bytes_val = remaining_bytes.load(Ordering::Acquire);
-                        let payload_len = event.payload.len();
+                            let zero_index = get_zero_byte_index(&event.payload).unwrap();
+                            current_file_transfer.buffer = event.payload.split_off(zero_index);
+                            current_file_transfer.received = current_file_transfer.buffer.len();
 
-                        if remaining_bytes_val >= payload_len {
-                            if let Some(stream) = file_stream.lock().unwrap().as_mut() {
-                                stream.write_all(&event.payload).unwrap();
-
-                                let new_remaining = remaining_bytes_val - payload_len;
-                                remaining_bytes.store(new_remaining, Ordering::Release);
-
-                                if new_remaining == 0 {
-                                    stream.flush().unwrap();
-                                    is_meta_data.store(true, Ordering::Release);
-                                }
-                            }
+                            let file = File::create(&current_file_transfer.metadata.as_mut().unwrap().name).unwrap();
+                            let perms = current_file_transfer.metadata.as_mut().unwrap().permissions.clone();
+                            let _ = file.set_permissions(perms);
+                            current_file_transfer.file_stream = Some(BufWriter::new(file));
                         } else {
-                            if let Some(stream) = file_stream.lock().unwrap().as_mut() {
-                                stream.write_all(&event.payload[0..remaining_bytes_val]).unwrap();
-                                let _ = stream.flush();
-                            }
+                            current_file_transfer.buffer.extend_from_slice(&event.payload);
+                        }
 
-                            let leftover_slice = &event.payload[remaining_bytes_val..];
-                            let mut guard = metadata_buffer.lock().unwrap();
+                    } else {
+                        current_file_transfer.received += event.payload.len();
+                        current_file_transfer.buffer.extend_from_slice(&event.payload);
 
-                            match leftover_slice.iter().position(|&b| b == 0) {
-                                Some(zero_byte_index) => {
-                                    guard.extend_from_slice(&leftover_slice[..zero_byte_index]);
-                                    let json = String::from_utf8_lossy(&guard);
+                        if current_file_transfer.received >= current_file_transfer.metadata.as_mut().unwrap().size {
+                            let _ = current_file_transfer.file_stream.as_mut().unwrap().write_all(&current_file_transfer.buffer[0..current_file_transfer.metadata.as_mut().unwrap().size]);
+                            let _ = current_file_transfer.file_stream.as_mut().unwrap().flush();
 
-                                    if let Ok(meta_data) = json::parse(&json) {
-                                        let mut new_size = 0;
-
-                                        if let Some(size_val) = meta_data["size"].as_usize() {
-                                            new_size = size_val;
-                                        }
-                                        if let Some(name_val) = meta_data["name"].as_str() {
-                                            let file = File::create(name_val).unwrap();
-
-                                            if let Some(is_app_val) = meta_data["is_app"].as_bool() {
-                                                if is_app_val {
-                                                    file.set_permissions(Permissions::from_mode(0o111)).expect("Failed to set permissions");
-                                                }
-                                            }
-
-                                            let mut writer = BufWriter::new(file);
-
-                                            let new_file_data_start = zero_byte_index + 1;
-
-                                            if new_file_data_start < leftover_slice.len() {
-                                                let data_for_new_file = &leftover_slice[new_file_data_start..];
-                                                writer.write_all(data_for_new_file).unwrap();
-
-                                                let written = data_for_new_file.len();
-
-                                                if written >= new_size {
-                                                    writer.flush().unwrap();
-                                                    is_meta_data.store(true, Ordering::Release);
-                                                    remaining_bytes.store(0, Ordering::Release);
-                                                } else {
-                                                    remaining_bytes.store(new_size - written, Ordering::Release);
-                                                }
-                                            } else {
-                                                remaining_bytes.store(new_size, Ordering::Release);
-                                            }
-
-                                            *file_stream.lock().unwrap() = Some(writer);
-                                        }
-                                    }
-
-                                    guard.clear();
-                                },
-                                None => {
-                                    guard.extend_from_slice(leftover_slice);
-                                }
-                            }
+                            current_file_transfer.buffer = current_file_transfer.buffer.split_off(current_file_transfer.received);
+                            is_meta_data.store(true, Ordering::Release);
                         }
                     }
                 }
@@ -165,4 +117,39 @@ pub fn connect() -> bool {
     conn.close();
 
     true
+}
+
+fn get_meta_data(slice: &[u8]) -> Option<Metadata> {
+    let size;
+    let name;
+    let permissions;
+
+    match get_zero_byte_index(slice) {
+        Some(zero_byte_index) => {
+            let json = String::from_utf8_lossy(&slice[..zero_byte_index]);
+
+            if let Ok(meta_data) = json::parse(&json)
+            && let Some(name_val) = meta_data["name"].as_str()
+            && let Some(size_val) = meta_data["size"].as_usize()
+            && let Some(is_app_val) = meta_data["is_app"].as_bool() {
+                size = size_val;
+                name = name_val.to_string();
+
+                if is_app_val {
+                    permissions = Permissions::from_mode(0o111);
+                } else {
+                    permissions = Permissions::from_mode(0o000);
+                }
+
+                return Option::from(Metadata { size, name, permissions });
+            }
+        },
+        _ => {}
+    }
+
+    None
+}
+
+fn get_zero_byte_index(slice: &[u8]) -> Option<usize> {
+    slice.iter().position(|&b| b == 0)
 }
